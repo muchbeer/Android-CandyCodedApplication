793775db48e9deb08e86a1e46398cbd1b321145c
muchbeer <gman2them@gmail.com>
2021-12-05 21:53:56 +0300

    the updated project phase finish phase one fail

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -74,12 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        Intent infoIntent = new Intent(this, InfoActivity.class);
-        startActivity(infoIntent);
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
